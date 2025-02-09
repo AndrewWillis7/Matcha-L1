747ef6261a9d83571c9675fe6553b8dae0b0db1e
AndrewWillis7 <AndrewWillis771@outlook.com>
2025-02-08 23:44:31 -0800

    window

---
+++ src/core.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import tkinter as tk
+
+from tkinter import messagebox
+
+# Paths
+flair_path = os.path.abspath("./lib/flair")
+pyqt_path = os.path.abspath("./lib/pyqt")
+
+# Appending
+sys.path.append(flair_path)
+sys.path.append(pyqt_path)
+
+# local Distros
+#import flair.models
+#import flair.data
+
+
