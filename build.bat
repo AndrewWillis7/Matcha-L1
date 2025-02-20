bec34977373f9d6b82c7b244875ecf0bb2a96e38
AndrewWillis7 <AndrewWillis771@outlook.com>
2025-02-19 17:34:02 -0800

    Build script functional for venv

--- build.bat
+++ build.bat
@@ -1,22 +1,51 @@
 @echo off
+
+echo Project file location: %~dp0
+
 echo Running Python Pack Initialization
+color 0E
+timeout /t 2 /nobreak >nul
 
-echo installing Git Submodules
+if not exist ".\generated_environment" (
+    echo Generating Virtual Environment
+    color 07
+    color 0C
+    echo [DO NOT CLOSE]
 
-call git submodule init
-call git submodule update
-if errorlevel 1 (
-    echo Failed to Initialize or Update Submodule.
-    exit /b 1
+    py -m venv %~dp0generated_environment
+) else (
+    color 0E
+    echo Environment already exists
 )
+color 07
+
+color 0E
+echo ACTIVATING ENVIRONMENT 
+color 0C
+echo [DO NOT CLOSE]
+color 07
+
+call generated_environment\Scripts\activate.bat
+
+color 0A
+echo INSTALLING PIP MODULES
+
+:: Installed Pip Modules
+py -m pip install customtkinter
+py -m pip install nltk
+
+color 07
+echo Installed pip modules
 
 echo Creating Log
 if not exist ".\log.txt" (
     echo. > ".\log.txt"
     echo Created Log File
 ) else (
+    color 0E
     echo Log File already exists
 )
 
+color 0E
 echo Setup Complete!!
 pause
--- src/EntryPoint.py
+++ src/EntryPoint.py
@@ -1,5 +1,6 @@
 from core import *
 
+import ui_control
 
 app = ui_control.create_window()
 app.mainloop()
