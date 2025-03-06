bee0221f170c281e8679858f81c508dd898671d1
AndrewWillis7 <andrewwillis771@outlook.com>
2025-03-05 23:51:41 -0800

    Build Optimization

--- src/core.py
+++ src/core.py
@@ -4,15 +4,7 @@ import threading
 import time
 
 import customtkinter as ctk
-import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from functools import wraps
-
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from PIL import Image, ImageTk
-
-import tkinterhtml
-from markdown_it import MarkdownIt
 
 import api
